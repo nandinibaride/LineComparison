9b94f83fdd8aec7f20b3a319d7854bb91dfca117
nandinibaride <nandinibaride1234@gmail.com>
2021-08-16 19:21:23 +0530

    Modified: give input from user

--- src/main/java/linecomparison/com/CalculateLength.java
+++ src/main/java/linecomparison/com/CalculateLength.java
@@ -1,8 +1,17 @@
-package linecomparison.com;
 
+package linecomparison.com;
+import java.util.*;
 public class CalculateLength {
 	public static void main(String args[]) {
-		int x1 = 4 , x2 = 5 , y1 = 6 , y2 = 8;
+       Scanner sc= new Scanner(System.in);     
+		System.out.print("Enter x1  ");  
+		int x1 = sc.nextInt();  
+		System.out.print("Enter y1 ");  
+		int y1 = sc.nextInt();  
+		System.out.print("Enter x2 ");  
+		int x2 = sc.nextInt();  
+		System.out.print("Enter y2 ");  
+		int y2 = sc.nextInt();
 		double calculatelength = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 		System.out.println("length is " + calculatelength);
 	}
